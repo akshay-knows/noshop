6d69a15fd427048aad3f49e4d8976127b1bee0a2
akshay trivedi <Akshaytrivedi3.14@gmail.com>
2026-09-09 22:37:53 +0530

    Fix inventory Kafka bootstrap configuration

--- inventory-service/src/main/java/com/noshop/inventory_service/config/KafkaConsumerConfig.java
+++ inventory-service/src/main/java/com/noshop/inventory_service/config/KafkaConsumerConfig.java
@@ -3,6 +3,7 @@ package com.noshop.inventory_service.config;
 import com.noshop.inventory_service.event.ProductCreatedEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -16,6 +17,9 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ConsumerFactory<String, ProductCreatedEvent> productEventConsumerFactory() {
         JsonDeserializer<ProductCreatedEvent> deserializer =
@@ -24,7 +28,7 @@ public class KafkaConsumerConfig {
         deserializer.setUseTypeHeaders(false);
 
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "${spring.kafka.bootstrap-servers}");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "inventory-service");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
