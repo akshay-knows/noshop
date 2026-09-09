d08c4992a6a71b9462ec7c716e13e888f6111876
akshay trivedi <Akshaytrivedi3.14@gmail.com>
2026-09-09 22:36:52 +0530

    Add product created event contract

---
+++ inventory-service/src/main/java/com/noshop/inventory_service/event/ProductCreatedEvent.java
@@ -0,0 +1,20 @@
+package com.noshop.inventory_service.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductCreatedEvent {
+
+    private Long productId;
+    private String name;
+    private String status;
+    private List<ProductVariantEvent> variants;
+}
