903c47a056eaf0d9a404bb2d288787650d055365
akshay trivedi <Akshaytrivedi3.14@gmail.com>
2026-09-09 22:37:03 +0530

    Add product variant event contract

---
+++ inventory-service/src/main/java/com/noshop/inventory_service/event/ProductVariantEvent.java
@@ -0,0 +1,21 @@
+package com.noshop.inventory_service.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductVariantEvent {
+
+    private Long variantId;
+    private String sku;
+    private BigDecimal packSize;
+    private String unit;
+    private BigDecimal price;
+}
