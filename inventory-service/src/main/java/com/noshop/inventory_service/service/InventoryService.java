f6c7d07049e047b035dda0b82786f868a7c2ce50
akshay trivedi <Akshaytrivedi3.14@gmail.com>
2026-09-09 22:35:07 +0530

    Complete inventory service operations

--- inventory-service/src/main/java/com/noshop/inventory_service/service/InventoryService.java
+++ inventory-service/src/main/java/com/noshop/inventory_service/service/InventoryService.java
@@ -1,13 +1,24 @@
 package com.noshop.inventory_service.service;
 
 import com.noshop.inventory_service.dto.request.InventoryRequest;
+import com.noshop.inventory_service.dto.request.InventoryUpdateRequest;
 import com.noshop.inventory_service.dto.response.InventoryResponse;
+
+import java.util.List;
 
 public interface InventoryService {
 
     InventoryResponse createInventory(InventoryRequest request);
 
     InventoryResponse getInventory(Long variantId, Long warehouseId);
+
+    List<InventoryResponse> getInventoryByVariant(Long variantId);
+
+    List<InventoryResponse> getInventoryByWarehouse(Long warehouseId);
+
+    InventoryResponse updateInventory(Long id, InventoryUpdateRequest request);
+
+    void deleteInventory(Long id);
 
     void addStock(Long variantId, Long warehouseId, Integer quantity);
 
