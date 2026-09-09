d8cbd9a85fa62bce137b7fe60ef65c61c14e4425
akshay trivedi <Akshaytrivedi3.14@gmail.com>
2026-09-09 19:40:59 +0530

    fix redis cache configuration bean

--- product-service/src/main/java/com/noshop/product_service/config/RedisConfig.java
+++ product-service/src/main/java/com/noshop/product_service/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.noshop.product_service.config;
 
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -12,11 +13,21 @@ import java.time.Duration;
 @Configuration
 @EnableCaching
 public class RedisConfig {
+
+    @Bean
     public RedisCacheConfiguration cacheConfiguration() {
         return RedisCacheConfiguration.defaultCacheConfig()
-                                      .entryTtl(Duration.ofMinutes(10))
-                                      .disableCachingNullValues()
-                                      .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                                      .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new StringRedisSerializer()
+                        )
+                )
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()
+                        )
+                );
     }
 }
