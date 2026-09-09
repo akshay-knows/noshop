9de3765f0cae7e4e655a6d89f28f00c5bff3ac79
akshay trivedi <Akshaytrivedi3.14@gmail.com>
2026-09-09 19:41:03 +0530

    allow retry of failed outbox events

--- product-service/src/main/java/com/noshop/product_service/outbox/OutboxEventRepository.java
+++ product-service/src/main/java/com/noshop/product_service/outbox/OutboxEventRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface OutboxEventRepository extends JpaRepository<OutboxEvent, Long> {
 
-    List<OutboxEvent> findTop100ByStatusOrderByCreatedAtAsc(
-            OutboxStatus status
+    List<OutboxEvent> findTop100ByStatusInOrderByCreatedAtAsc(
+            List<OutboxStatus> statuses
     );
 }
