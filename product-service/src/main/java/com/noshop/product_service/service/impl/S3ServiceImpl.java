cd497ea560c76e7c46abb525dc09d924f20cecf8
akshay trivedi <Akshaytrivedi3.14@gmail.com>
2026-09-10 20:21:12 +0530

    Fix S3 service import and fallback

--- product-service/src/main/java/com/noshop/product_service/service/impl/S3ServiceImpl.java
+++ product-service/src/main/java/com/noshop/product_service/service/impl/S3ServiceImpl.java
@@ -9,7 +9,6 @@ import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.presigner.S3PresignRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
